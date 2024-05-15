100fb11c7f3f37c6cb62925b8e5db8884b7dbf04
LeiderIP <leiderpaez13@gmail.com>
2024-05-15 09:26:57 -0500

    first commit, main page created

---
+++ Slider.js
@@ -0,0 +1,35 @@
+const slideLeftButton = document.getElementById('slide-left-button');
+const slideRightButton = document.getElementById('slide-right-button');
+const optionContainers = document.querySelectorAll('.option-container');
+const totalOptions = optionContainers.length;
+const optionsToShow = 3;
+let currentIndex = 0;
+
+function showOptions() {
+    optionContainers.forEach((container, index) => {
+        if (index >= currentIndex && index < currentIndex + optionsToShow) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
+    });
+}
+
+showOptions();
+
+slideLeftButton.addEventListener('click', () => {
+    currentIndex -= optionsToShow;
+    if (currentIndex < 0) {
+        currentIndex = totalOptions - optionsToShow; 
+    }
+    showOptions();
+});
+
+
+slideRightButton.addEventListener('click', () => {
+    currentIndex += optionsToShow;
+    if (currentIndex >= totalOptions) {
+        currentIndex = 0; 
+    }
+    showOptions();
+});
